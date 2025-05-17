d447cf1eaa00d57d9d7458351842b9ff3c9ddb58
SaidmahmudMars <abdusamatovsaidmahmud@gmail.com>
2025-05-17 11:58:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let ism = prompt('ismin yoz')
+let login = +prompt('space login yoz')
+let password = prompt('space password yoz')
+
+
+const obj = {
+    names: ism,
+    nikname: login,
+    parol: password,  
+}
+
+
+
+
+
+
+
+console.log(obj);
+
+
+
+
+
